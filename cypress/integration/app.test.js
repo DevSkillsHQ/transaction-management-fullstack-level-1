93b9a3c13435106e8e7e2b082051225a936bf44c
Anton Fenske <anton@devskills.co>
2020-10-27 16:35:41 +0100

    Update app.test.js

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -2,25 +2,22 @@ describe('Account Management Frontend - Level 1', () => {
   it('successfully loads, submits, and renders transactions', () => {
     cy.visit('/')
 
-    cy.get('[data-cy=accountId]').type('70ad2f95-aa52-4e04-a085-c5cc2a4d4ee4')
-    cy.get('[data-cy=amount]').type('30')
-    cy.get('[data-cy=form]').submit()
+    cy.get('[data-type=account-id]').type('70ad2f95-aa52-4e04-a085-c5cc2a4d4ee4')
+    cy.get('[data-type=amount]').type('30')
+    cy.get('[data-type=transaction-form]').submit()
 
-    cy.get('dt').first().contains('Transferred $30 to 70ad2f95-aa52-4e04-a085-c5cc2a4d4ee4.')
-    cy.get('dt').first().contains('Current 70ad2f95-aa52-4e04-a085-c5cc2a4d4ee4\'s balance is $30.')
+    cy.get('[data-type=transaction]').first().get('[data-account-id=70ad2f95-aa52-4e04-a085-c5cc2a4d4ee4][data-amount=30][data-balance=30]').should('exist')
 
-    cy.get('[data-cy=accountId]').type('aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4')
-    cy.get('[data-cy=amount]').type('7')
-    cy.get('[data-cy=form]').submit()
+    cy.get('[data-type=account-id]').type('aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4')
+    cy.get('[data-type=amount]').type('7')
+    cy.get('[data-type=transaction-form]').submit()
 
-    cy.get('dt').first().contains('Transferred $7 to aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4.')
-    cy.get('dt').first().contains('Current aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4\'s balance is $7.')
+    cy.get('[data-type=transaction]').first().get('[data-account-id=aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4][data-amount=7][data-balance=7]').should('exist')
 
-    cy.get('[data-cy=accountId]').type('aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4')
-    cy.get('[data-cy=amount]').type('-7')
-    cy.get('[data-cy=form]').submit()
+    cy.get('[data-type=account-id]').type('aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4')
+    cy.get('[data-type=amount]').type('-7')
+    cy.get('[data-type=transaction-form]').submit()
  
-    cy.get('dt').first().contains('Withdrew $7 from aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4.')
-    cy.get('dt').first().contains('Current aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4\'s balance is $0.')
+    cy.get('[data-type=transaction]').first().get('[data-account-id=aaad2f95-aa52-4e04-a085-c5cc2a4d4ee4][data-amount=-7][data-balance=0]').should('exist')
   })
 })
